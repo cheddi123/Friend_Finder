1f9eff9127f51710ab5b21cb647edd171d810894
Cheddi Charles <cheddi.charles@gmail.com>
2018-11-20 09:03:11 -0600

    working on validing the user input

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,59 +1,60 @@
 
+// get the static array from the data friends
 var friendsData = require("../data/friends")
 
- 
+
 console.log(friendsData)
 module.exports = function (app) {
 
     // Get a total of friends entries
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
-       
+
     });
 
     // post new entry of friend
     app.post("/api/friends", function (req, res) {
-        
+
 
         //gets access friends list data
         var userArray = req.body;
-        var userResponse = userArray.scores
+        var userScore = userArray.scores
 
         var matchName = '';
         var matchImage = '';
 
         var limitDiff = 60;
-      
+
 
         for (var i = 0; i < friendsData.length; i++) {
             var difference = 0;
 
-            for (var j = 0; j < userResponse.length; j++) {
+            for (var j = 0; j < userScore.length; j++) {
 
-                difference += Math.abs(friendsData[i].scores[j] - userResponse[j]);
+                difference += Math.abs(friendsData[i].scores[j] - userScore[j]);
             }
 
-            
+
 
             if (difference <= limitDiff) {
                 limitDiff = difference;
                 matchName = friendsData[i].name;
                 matchImage = friendsData[i].photo;
-             console.log("the diffenrece is : " + difference)
+                console.log("the diffenrece is : " + difference)
             }
             console.log("your best match is " + matchName)
             console.log("The image is  " + matchImage)
         }
 
-       //push new friend data into friends array
+        //push new friend data into friends array
         friendsData.push(userArray);
 
- // after matching , send this data to the client in modal display
-           res.json({
-            status:"ok",
-           name : matchName, 
-          photo :matchImage
-         });
+        // after matching , send this data to the client in modal display
+        res.json({
+            status: "ok",
+            name: matchName,
+            photo: matchImage
+        });
 
     });
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,11 +8,12 @@ var path = require("path");
 module.exports=function(app){
 
 
-
+// upload home page at this route 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
+  // route directory to survey page
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
